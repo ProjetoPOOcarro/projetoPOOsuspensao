5b9b3c7509a869217378773215534df28df023d2
davidLu37 <davidluisleitedeoliveira22@gmail.com>
2024-12-18 13:58:45 -0300

    Create Main.java

---
+++ Programa/src/InterfaceGráfica/src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	 @Override
+	    public void start(Stage primaryStage) { // Método chamado pelo JavaFX ao iniciar
+	        Label label = new Label("Olá, JavaFX!");
+	        StackPane root = new StackPane(label);
+	        
+	        Scene scene = new Scene(root, 1000, 200);
+	        primaryStage.setTitle("Teste JavaFX");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args); // Inicializa a aplicação JavaFX
+	}
+}
