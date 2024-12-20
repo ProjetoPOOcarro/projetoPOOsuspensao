5e3fdc43bb3cdc8025194ffb68a3a02ed36db24b
4t4p0 <lucasyuukisasaki@gmail.com>
2024-12-20 12:11:57 -0300

    Adicionei comentarios no programa

--- Programa/src/InterfaceGráfica/src/application/SistemaDeSuspensao.java
+++ Programa/src/InterfaceGráfica/src/application/SistemaDeSuspensao.java
@@ -2,11 +2,11 @@ package InterfaceGráfica.src.application;
 
 public class SistemaDeSuspensao {
 
-	//Atributos
+	// Atributos (Siglas estão baseadas no desenho)
 	double MassaSuspensa;
-	double MassaNaoSuspensa;
-	Mola mola;
-	Amortecedor amortecedor;
+	double MassaNaoSuspensa; // (Eixo e suspensão) (mu)
+	Mola mola; // (ks)
+	Amortecedor amortecedor; // (cs)
 	Massa massa;
-	
+
 }
