db4852de4242e1ccc32af73dfbd19172fc6d54c2
4t4p0 <lucasyuukisasaki@gmail.com>
2024-12-22 18:32:37 -0300

    Comecei a fazer a função da posição do carro

--- Programa/src/InterfaceGráfica/src/application/SistemaDeSuspensao.java
+++ Programa/src/InterfaceGráfica/src/application/SistemaDeSuspensao.java
@@ -3,10 +3,17 @@ package InterfaceGráfica.src.application;
 public class SistemaDeSuspensao {
 
 	// Atributos (Siglas estão baseadas no desenho)
-	double MassaSuspensa;
+	double MassaSuspensa; // (Carro)
 	double MassaNaoSuspensa; // (Eixo e suspensão) (mu)
 	Mola mola; // (ks)
 	Amortecedor amortecedor; // (cs)
 	Massa massa;
+	
+	
+	//******AINDA FAZENDO********
+	double PosicaoMassaSuspensa() {
+		
+		
+	}
 
 }
