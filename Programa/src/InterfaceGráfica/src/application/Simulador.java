225c2a982936fedb4406cce0d434e706fcd26707
davidLu37 <davidluisleitedeoliveira22@gmail.com>
2024-12-27 18:16:52 -0300

    Simulador(classe que administra as telas).java

--- Programa/src/InterfaceGráfica/src/application/Simulador.java
+++ Programa/src/InterfaceGráfica/src/application/Simulador.java
@@ -5,28 +5,31 @@ import javafx.stage.Stage;
 
 public class Simulador {
     private static String[] Textos = {
-        "Digite o valor da rigidez da mola (N/m):",
+        "Digite o valor da rigidez da suspensão (N/m):",
         "Digite o valor da massa suspensa (kg):",
         "Digite o valor da massa não suspensa (kg):",
         "Digite o coeficiente de amortecimento (Ns/m):",
-        "Digite a rigidez do pneu (N/m):",
-        "Digite o deslocamento imputado pelo solo (m):"
+        "Digite a rigidez do pneu (N/m):"
     };
 
-    private final float[] valores = new float[6];
+    private static double[] valores = new double[5];
     private int contador = 0;
 
-    public void atribuirValor(int aux, float valor) {
-        valores[aux] = valor;
-        contador++;
+    public void atribuirValor(int aux, double valor) {
+    	//0 valor da rigidez da suspensão (Ks), 1 valor da massa suspensa(Kg), 
+    	 //2 valor da massa não suspensa(Kg),3 o coeficiente de amortecimento (Cs),
+    	 //4 a rigidez do pneu (Kt)
+    	valores[aux] = valor;
+    	contador++;
+    
     }
 
     public boolean todosValoresDefinidos() {
-        return contador == 6;
+    	return contador == 5;
     }
 
     public void abrirSegundaTela(Stage animação) {
-        animação simulador = new animação();
+    	animação simulador = new animação();
         Scene cena2 = simulador.criarAnimação();
         animação.setTitle("Simulador de um quarto de suspensão veicular");
         animação.setScene(cena2);
@@ -35,5 +38,8 @@ public class Simulador {
     }
     public static String getTexto(int aux) {
     	return Textos[aux];
+    }
+    public static double getValores(int aux) {
+    	return valores[aux];
     }
 }
