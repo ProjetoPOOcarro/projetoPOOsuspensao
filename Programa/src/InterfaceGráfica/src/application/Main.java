9e999e867bd2481ed795afab18eeab8b108080f4
davidLu37 <davidluisleitedeoliveira22@gmail.com>
2024-12-27 12:19:17 -0300

    Update Main.java

--- Programa/src/InterfaceGráfica/src/application/Main.java
+++ Programa/src/InterfaceGráfica/src/application/Main.java
@@ -1,24 +1,21 @@
-package InterfaceGráfica.src.application;
-	
+package application;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	 @Override
-	    public void start(Stage primaryStage) { // Método chamado pelo JavaFX ao iniciar
-	        Label label = new Label("Olá, JavaFX!");
-	        StackPane root = new StackPane(label);
-	        
-	        Scene scene = new Scene(root, 1000, 200);
-	        primaryStage.setTitle("Teste JavaFX");
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-	}
 
-	public static void main(String[] args) {
-		launch(args); // Inicializa a aplicação JavaFX
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        PrimeiraTela interfaceSuspensao = new PrimeiraTela();
+        Scene cena = interfaceSuspensao.criarCena(primaryStage);
+        primaryStage.setTitle("Simulador de um quarto de suspensão veicular");
+        primaryStage.setScene(cena);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
