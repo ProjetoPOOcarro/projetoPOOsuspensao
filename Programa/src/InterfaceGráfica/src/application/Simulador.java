483cfde64d4d23bba9c91205a14f63475d57a121
davidLu37 <davidluisleitedeoliveira22@gmail.com>
2025-01-01 14:06:08 -0300

    Update Simulador.java

--- Programa/src/InterfaceGráfica/src/application/Simulador.java
+++ Programa/src/InterfaceGráfica/src/application/Simulador.java
@@ -21,20 +21,34 @@ public class Simulador {
     	 //4 a rigidez do pneu (Kt)
     	valores[aux] = valor;
     	contador++;
-    
     }
 
     public boolean todosValoresDefinidos() {
-    	return contador == 5;
+    	return contador == 4;
     }
 
-    public void abrirSegundaTela(Stage animação) {
+    public void abrirSegundaTela() {
+    	Stage animação = new Stage();
     	animação simulador = new animação();
         Scene cena2 = simulador.criarAnimação();
         animação.setTitle("Simulador de um quarto de suspensão veicular");
         animação.setScene(cena2);
         animação.setResizable(false); // Desativa o redimensionamento
         animação.show();
+        animação.setX(100);
+        animação.setY(100);
+        
+        Stage gráfico1 = new Stage();
+        Gráficos dados = new Gráficos();
+        Scene cena3 = dados.CriarGráfico1();
+        gráfico1.setTitle("Grafico1");
+        gráfico1.setScene(cena3);
+        gráfico1.setResizable(false); // Desativa o redimensionamento
+        gráfico1.show();
+        
+        gráfico1.setX(681);
+        gráfico1.setY(100);
+        
     }
     public static String getTexto(int aux) {
     	return Textos[aux];
