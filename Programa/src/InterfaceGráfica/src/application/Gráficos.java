dda73e56455a6d115c01d64da3024d8807b86a71
davidLu37 <davidluisleitedeoliveira22@gmail.com>
2025-01-04 11:22:53 -0300

    Update Gráficos.java

--- Programa/src/InterfaceGráfica/src/application/Gráficos.java
+++ Programa/src/InterfaceGráfica/src/application/Gráficos.java
@@ -8,8 +8,6 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 public class Gráficos {
@@ -19,8 +17,8 @@ public class Gráficos {
     public Scene CriarGráfico1() {
         NumberAxis eixoX = new NumberAxis();
         NumberAxis eixoY = new NumberAxis();
-        eixoX.setLabel("Tempo");
-        eixoY.setLabel("Deslocamento da massa suspensa");
+        eixoX.setLabel("Tempo(s)");
+        eixoY.setLabel("Deslocamento da massa suspensa(m)");
 
         // Criando o gráfico de linha
         LineChart<Number, Number> graficoDeLinha = new LineChart<>(eixoX, eixoY);
@@ -29,20 +27,15 @@ public class Gráficos {
         // Criando a série de dados
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
         series.setName("Valores da Variável");
-
+    	
         // Adicionando a série ao gráfico
         graficoDeLinha.getData().add(series);
-
-        // Texto que exibe o valor atual de DADOS
-        Text textoPosicaoY = new Text(10, 20, "Posição Y: " + DADOS);
-        textoPosicaoY.setFill(Color.BLACK);
-
+     
         // Usar um Timeline para atualizar o gráfico em tempo real
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.5), e -> {
             // Atualizar o gráfico com o novo valor de DADOS
             Platform.runLater(() -> {
                 series.getData().add(new XYChart.Data<>(tempo, DADOS));
-                textoPosicaoY.setText("Posição Y: " + DADOS);
             });
 
             tempo += 0.5; // Incrementar o tempo para o próximo ponto no eixo X
@@ -53,12 +46,12 @@ public class Gráficos {
 
         // Adicionar o texto e o gráfico ao painel
         Pane painel = new Pane();
-        painel.getChildren().addAll(textoPosicaoY, graficoDeLinha);
-        Scene cena = new Scene(painel, 580, 320);
+        painel.getChildren().addAll(graficoDeLinha);
+        Scene cena = new Scene(painel, 580, 400);
         return cena;
     }
 
     public static void setDados(double aux) {
-        DADOS = aux*-1;
+        DADOS = (aux*-1.0)/10.0;
     }
 }
