06ba3ecaf179646950296f4cf3b8bac97015d722
4t4p0 <lucasyuukisasaki@gmail.com>
2025-01-04 21:45:18 -0300

    Update SistemaDeSuspensao.java

--- Programa/src/InterfaceGráfica/src/application/SistemaDeSuspensao.java
+++ Programa/src/InterfaceGráfica/src/application/SistemaDeSuspensao.java
@@ -21,7 +21,17 @@ public class SistemaDeSuspensao {
     double c_s;
     double A_sin;
     double dt = 0.01; // Passo de tempo (s)
-    int steps = 500; // Número de passos (5 s com passo de 0,01 s)
+    int passo = 1000; // Número de passos (5 s com passo de 0,01 s)
+    
+    double [][] INF_SISTEMA_SUSPENSAO = new double[passo][5];
+    /*
+     * [passo][0] INF_SISTEMA_SUSPENSAO = Tempo
+     * [passo][1] INF_SISTEMA_SUSPENSAO = Deslocamento da massa suspensa
+     * [passo][2] INF_SISTEMA_SUSPENSAO = Deslocamento da massa não suspensa
+     * [passo][3] INF_SISTEMA_SUSPENSAO = Deslocamento do sistema (Deslocamento da massa não suspensa - Deslocamento da massa suspensa)
+     * [passo][4] INF_SISTEMA_SUSPENSAO = Aceleração da massa suspensa
+     */
+    
 
     // Seta os parâmetros do sistema de suspensão
     public void setParametros(Amortecedor Amortecedor, Mola MolaSuspensao, Mola MolaPneu, Massa MassaSuspensa, Massa MassaNaoSuspensa, Estrada Estrada) {
@@ -59,21 +69,22 @@ public class SistemaDeSuspensao {
                 {0, 0, 1, 0}
         };
 
-        double[] Tempo = new double[steps];
-        double[] Oscilação = new double[steps];
-        for (int i = 0; i < steps; i++) {
-            Tempo[i] = i * dt;
+        double[] Tempo = new double[passo];
+        double[] Oscilação = new double[passo];
+        for (int i = 0; i < passo; i++) {
+        	INF_SISTEMA_SUSPENSAO [i][0] = i * dt;
+        	Tempo[i] = i * dt;
             Oscilação[i] = Estrada.OscilacaoEstrada(Tempo[i]);
         }
 
         double[] x = {0, 0, 0, 0};
-        double[][] Deslocamento = new double[steps][2];
+        double[][] Deslocamento = new double[passo][2];
 
         RealMatrix AMatrix = new Array2DRowRealMatrix(A);
         RealMatrix BMatrix = new Array2DRowRealMatrix(B);
         RealMatrix CMatrix = new Array2DRowRealMatrix(C);
 
-        for (int i = 0; i < steps; i++) {
+        for (int i = 0; i < passo; i++) {
             RealMatrix xMatrix = new Array2DRowRealMatrix(x);
             RealMatrix OscilaçãoMatrix = new Array2DRowRealMatrix(new double[]{Oscilação[i]});
 
@@ -85,14 +96,33 @@ public class SistemaDeSuspensao {
             RealMatrix dx = k1.add(k2.scalarMultiply(2)).add(k3.scalarMultiply(2)).add(k4).scalarMultiply(dt / 6);
 
             x = xMatrix.add(dx).getColumn(0);
-
+            
+            System.out.println("b");
+            for (int c = 0; c < x.length; c++) {
+                System.out.println(x[c]); 
+            System.out.println("a");// Imprime cada elemento do array
+            }
+            
             RealMatrix DeslocamentoMatrix = CMatrix.multiply(new Array2DRowRealMatrix(x));
 
             Deslocamento[i][0] = DeslocamentoMatrix.getEntry(0, 0);
+            setINF_SISTEMA_SUSPENSAO_Desl_Massa_Sus(i, Deslocamento[i][0]);
             MassaSuspensa.setPosição(Deslocamento[i][0]);
+            
             Deslocamento[i][1] = DeslocamentoMatrix.getEntry(1, 0);
+            setINF_SISTEMA_SUSPENSAO_Desl_Massa_N_Sus(i, Deslocamento[i][1]);
             MassaNaoSuspensa.setPosição(Deslocamento[i][1]);
+            
+            setINF_SISTEMA_SUSPENSAO_Diferença_Desl(i, Deslocamento[i][1]-Deslocamento[i][0]);
+            
+            
 
+            
+            double acel = (k_s * (x[2] - x[0]) + c_s * (x[3] - x[2])) / m_s;
+            
+            setINF_SISTEMA_SUSPENSAO_Aceleracao_Sus(i, acel);
+            
+            
             if (getDeslocamentoMAX_SUS() < Deslocamento[i][0]) {
             	setDeslocamentoMAX_SUS(Deslocamento[i][0]);
             }
@@ -123,4 +153,74 @@ public class SistemaDeSuspensao {
     public void setDeslocamentoMAX_N_SUS(double deslocamentoMAX_N_SUS) {
         this.DeslocamentoMAX_N_SUS = deslocamentoMAX_N_SUS;
     }
+    
+    //setters
+    public void setINF_SISTEMA_SUSPENSAO_Tempo(int passo, double Tempo) {
+    	this.INF_SISTEMA_SUSPENSAO[passo][0] = Tempo;
+    }
+    public void setINF_SISTEMA_SUSPENSAO_Desl_Massa_Sus(int passo, double Desl_Massa_Sus) {
+    	this.INF_SISTEMA_SUSPENSAO[passo][1] = Desl_Massa_Sus;
+    }
+    public void setINF_SISTEMA_SUSPENSAO_Desl_Massa_N_Sus(int passo, double Desl_Massa_N_Sus) {
+    	this.INF_SISTEMA_SUSPENSAO[passo][2] = Desl_Massa_N_Sus;
+    }
+    public void setINF_SISTEMA_SUSPENSAO_Diferença_Desl(int passo, double Diferença_Desl) {
+    	this.INF_SISTEMA_SUSPENSAO[passo][3] = Diferença_Desl;
+    }
+    public void setINF_SISTEMA_SUSPENSAO_Aceleracao_Sus(int passo, double Aceleracao_Sus) {
+    	this.INF_SISTEMA_SUSPENSAO[passo][4] = Aceleracao_Sus;
+    }
+    
+    //getters
+    public double getINF_SISTEMA_SUSPENSAO_Tempo(int passo) {
+        return this.INF_SISTEMA_SUSPENSAO[passo][0];
+    }
+
+    public double getINF_SISTEMA_SUSPENSAO_Desl_Massa_Sus(int passo) {
+        return this.INF_SISTEMA_SUSPENSAO[passo][1];
+    }
+
+    public double getINF_SISTEMA_SUSPENSAO_Desl_Massa_N_Sus(int passo) {
+        return this.INF_SISTEMA_SUSPENSAO[passo][2];
+    }
+
+    public double getINF_SISTEMA_SUSPENSAO_Diferença_Desl(int passo) {
+        return this.INF_SISTEMA_SUSPENSAO[passo][3];
+    }
+
+    public double getINF_SISTEMA_SUSPENSAO_Aceleracao_Sus(int passo) {
+        return this.INF_SISTEMA_SUSPENSAO[passo][4];
+    }
+    
+    /*
+    public static void main(String[] args) {
+        Amortecedor amortecedor = new Amortecedor();
+        amortecedor.setConstanteC(1000);
+        Mola molaSuspensao = new Mola();
+        molaSuspensao.setConstanteK(15000);
+        Mola molaPneu = new Mola();
+        molaPneu.setConstanteK(200000);
+        Massa massaSuspensa = new Massa();
+        massaSuspensa.setMassa(250);
+        Massa massaNaoSuspensa = new Massa();
+        massaNaoSuspensa.setMassa(5);
+        Estrada estrada = new Estrada(0.1);
+
+        SistemaDeSuspensao sis = new SistemaDeSuspensao();
+        sis.setParametros(amortecedor, molaSuspensao, molaPneu, massaSuspensa, massaNaoSuspensa, estrada);
+
+        sis.Calcular();
+
+        System.out.printf(" \n%.3f", sis.getDeslocamentoMAX_N_SUS());
+
+        // Altere o limite do laço de 1000 para 999 para garantir que não ultrapasse o índice máximo
+        for (int i = 0; i < 1000; i++) {
+            System.out.printf("\nTempo: %.3f", sis.getINF_SISTEMA_SUSPENSAO_Tempo(i));
+           // System.out.printf("***%.3f", sis.getINF_SISTEMA_SUSPENSAO_Desl_Massa_Sus(i));
+            //System.out.printf("***%.3f", sis.getINF_SISTEMA_SUSPENSAO_Desl_Massa_N_Sus(i));
+            //System.out.printf("***%.3f", sis.getINF_SISTEMA_SUSPENSAO_Diferença_Desl(i));
+            System.out.printf(" Aceleração: %.3f", sis.getINF_SISTEMA_SUSPENSAO_Aceleracao_Sus(i));
+        }
+    }
+    */
 }
